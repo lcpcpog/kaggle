6413ab3c43a26022db4feebad2a97fe22d6b5aea
lucas penha <lcpcpog>
2025-11-11 02:01:33 -0300

    att

--- trabalho.py
+++ trabalho.py
@@ -13,4 +13,28 @@ def carregar_dados():
             materiais = json.load(f)
     else:
         materiais = {}
+def registrar_material():
+    tema = input('Tema principal: ')
+    subtema = input('Subtema (deixe vazio se não tiver): ')
+    titulo = input('Título do material: ')
+    tipo = input('Tipo do material (artigo,video e etc.): ')
+    link = input('Link ou referência: ')
+
+    material = {
+        'titulo': titulo,
+        'tipo': tipo,
+        'link': link
+    }
+
+    # Adiciona ao tema/subtema
+    if tema not in materiais:
+        materiais[tema] = {'materiais': [], 'subtemas': {}}
+
+    if subtema:
+        adicionar_subtema_recursivo(materiais[tema]['subtemas'], subtema, material) #adicionar_subtema_recursivo nome da funcao para vcs usarem ai
+    else:
+        materiais[tema]['materiais'].append(material)
+
+    salvar_dados()
+    print('Material registrado com sucesso :) \n')
 
